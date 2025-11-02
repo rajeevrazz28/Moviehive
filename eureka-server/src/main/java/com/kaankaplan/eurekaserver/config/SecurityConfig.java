11cd549caf66590b28f3860cb04ec075ea1516a7
Rajeev Ranjan <64395095+rajeevrazz28@users.noreply.github.com>
2025-11-02 15:34:36 +0530

    Add files via upload

---
+++ eureka-server/src/main/java/com/kaankaplan/eurekaserver/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.kaankaplan.eurekaserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .antMatchers("/eureka/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+}
